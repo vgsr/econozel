55d1a41ff671be6c6a52eab7e26c2617a6120530
Laurens Offereins <lmoffereins@gmail.com>
2016-10-05 16:23:19 +0200

    Add widget settings to the Recent Articles widget, enabling listing articles per Edition. Fixes #4.

--- includes/widgets/class-econozel-articles-widget.php
+++ includes/widgets/class-econozel-articles-widget.php
@@ -52,58 +52,131 @@ class Econozel_Articles_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 
-		// Define widget details
-		$instance = wp_parse_args( $instance, array(
-			'title'          => str_replace( 'Econozel ', '', $this->name ),
+		// Define widget arguments
+		$r = wp_parse_args( $instance, array(
+
+			// Widget details
+			'title'          => esc_html__( 'Recent Articles', 'econozel' ),
 			'description'    => false,
-			'none_found'     => esc_html__( 'It seems there have been no articles published yet.', 'econozel' ),
+			'none_found'     => esc_html__( 'There were no articles found.', 'econozel' ),
 
 			// Query args
 			'econozel_edition' => false,
 			'posts_per_page'   => 5,
-			'orderby'          => 'date',
-			'order'            => 'DESC'
 		) );
 
-		// Define widget details
-		$title       = $instance['title'];
-		$description = $instance['description'];
-		$none_found  = $instance['none_found'];
+		// Detect whether to query by the current Edition
+		if ( $r['econozel_edition'] ) {
+			$r['econozel_edition'] = is_numeric( $r['econozel_edition'] ) ? (int) $r['econozel_edition'] : econozel_get_edition_id();
+		}
 
-		// Remove query vars
-		unset( $instance['title'], $instance['description'], $instance['none_found'] );
+		// When querying by Edition, query all its Articles and override the title
+		if ( $r['econozel_edition'] ) {
+			$r['posts_per_page'] = -1;
+			$title = econozel_get_edition_title( $r['econozel_edition'] );
+
+			if ( $title ) {
+				$r['title'] = sprintf( esc_html__( 'Articles in %s', 'econozel' ), $title );
+			}
+		}
 
 		// Open widget 
 		echo $args['before_widget'];
-		if ( $title ) {
-			echo $args['before_title'] . $title . $args['after_title'];
+		if ( $r['title'] ) {
+			echo $args['before_title'] . $r['title'] . $args['after_title'];
 		}
 
+		// Remove unwanted query vars
+		unset( $r['title'] );
+
 		// Query Articles
-		if ( econozel_query_articles( $instance ) ) : ?>
+		if ( econozel_query_articles( $r ) ) :
 
-			<?php if ( $description ) : ?>
-
-			<em><?php echo is_string( $description ) ? $description : $this->widget_options['description']; ?></em>
-
+			// List description
+			if ( $r['description'] ) : ?>
+			<em><?php echo is_string( $r['description'] ) ? $r['description'] : $this->widget_options['description']; ?></em>
 			<?php endif; ?>
 
-			<ul>
-				<?php while ( econozel_has_articles() ) : econozel_the_article(); ?>
-
-				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-
-				<?php endwhile; ?>
-			</ul>
+			<?php printf( '<ul>%s</ul>', walk_page_tree( econozel()->article_query->posts, 0, econozel_get_article_id(), $r ) ); ?>
 
 		<?php else : ?>
 
-			<?php echo $none_found; ?>
+			<?php echo $r['none_found']; ?>
 
 		<?php endif;
 
 		// Close widget
 		echo $args['after_widget'];
+	}
+
+	/**
+	 * Handles updating the settings for the current Recent Articles widget instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $new_instance
+	 * @param array $old_instance
+	 * @return array Sanitized instance data
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = $old_instance;
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['posts_per_page'] = (int) $new_instance['posts_per_page'];
+
+		/**
+		 * Edition
+		 * 
+		 * Accept a term ID for the given Edition, or a boolean True 
+		 * for the current Edition.
+		 */
+		if ( isset( $new_instance['econozel_edition'] ) ) {
+			$instance['econozel_edition'] = is_numeric( $new_instance['econozel_edition'] ) ? (int) $new_instance['econozel_edition'] : wp_validate_boolean( $new_instance['econozel_edition'] );
+		} else {
+			$instance['econozel_edition'] = false;
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Output widget form elements
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $instance Current settings
+	 */
+	public function form( $instance ) {
+
+		// Define local variable(s)
+		$title            = isset( $instance['title'] ) ? $instance['title'] : '';
+		$posts_per_page   = isset( $instance['posts_per_page'] ) ? $instance['posts_per_page'] : 5;
+		$econozel_edition = isset( $instance['econozel_edition'] ) ? (bool) $instance['econozel_edition'] : false;
+
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'econozel_edition' ); ?>"><?php esc_html_e( 'Filter articles by edition:', 'econozel' ); ?></label>
+			<?php econozel_dropdown_editions( array(
+				'id'                  => $this->get_field_id( 'econozel_edition' ),
+				'name'                => $this->get_field_name( 'econozel_edition' ),
+				'selected'            => $econozel_edition,
+				'option_none_value'   => 'false', // Results in boolean False when run through `wp_validate_boolean()`
+				'show_option_current' => true
+			) ); ?>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>"/>
+			<span class="description"><?php esc_html_e( 'For an edition, the title will be overridden.', 'econozel' ); ?></span>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'posts_per_page' ); ?>"><?php esc_html_e( 'Number of articles to show:', 'econozel' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'posts_per_page' ); ?>" name="<?php echo $this->get_field_name( 'posts_per_page' ); ?>" value="<?php echo esc_attr( $posts_per_page ); ?>"/>
+			<span class="description"><?php esc_html_e( 'For an edition, all articles are shown.', 'econozel' ); ?></span>
+		</p>
+
+		<?php
 	}
 }
 endif;
