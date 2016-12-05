1edf81cc3c6c33abeca63dc0ff183f34ff6f93f0
Laurens Offereins <lmoffereins@gmail.com>
2016-12-05 23:11:34 +0100

    WP SEO: introduce full plugin breadcrumbs structure. Fixes #9

--- includes/extend/wordpress-seo.php
+++ includes/extend/wordpress-seo.php
@@ -49,6 +49,9 @@ class Econozel_WPSEO {
 		add_filter( "manage_{$article}_posts_columns", array( $this, 'admin_remove_columns' ), 99 );
 		add_filter( "manage_edit-{$edition}_columns",  array( $this, 'admin_remove_columns' ), 99 );
 		add_filter( "manage_edit-{$volume}_columns",   array( $this, 'admin_remove_columns' ), 99 );
+
+		// Breadcrumbs
+		add_filter( 'wpseo_breadcrumb_links', array( $this, 'breadcrumb_links' ) );
 	}
 
 	/** Public methods **************************************************/
@@ -73,6 +76,129 @@ class Econozel_WPSEO {
 		}
 
 		return $columns;
+	}
+
+	/**
+	 * Modify the collection of page crumb links
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $crumbs Breadcrumb links
+	 * @return array Breadcrumb links
+	 */
+	public function breadcrumb_links( $crumbs ) {
+
+		// Econozel page. Fully overwrite crumb paths
+		if ( is_econozel() ) {
+
+			// Define plugin crumb presets
+			$_crumbs = array(
+				'root' => array(
+					'text'       => esc_html_x( 'Econozel', 'root page title', 'econozel' ),
+					'url'        => econozel_get_root_url(),
+					'allow_html' => false,
+				),
+				'volumes' => array(
+					'text'       => esc_html_x( 'Volumes', 'volume archives breadcrumb title', 'econozel' ),
+					'url'        => econozel_get_volume_archive_url(),
+					'allow_html' => false,
+				),
+				'articles' => array(
+					'text'       => esc_html_x( 'Articles', 'article archives breadcrumb title', 'econozel' ),
+					'url'        => get_post_type_archive_link( econozel_get_article_post_type() ),
+					'allow_html' => false,
+				)
+			);
+
+			// Always append Econozel root just after home
+			array_splice( $crumbs, 1, 0, array( $_crumbs['root'] ) );
+
+			// Define local variable(s)
+			$last = count( $crumbs ) - 1;
+
+			// Volume archive
+			if ( econozel_is_volume_archive() ) {
+
+				// Append Volume crumb, 'cause there is none
+				$crumbs[] = $_crumbs['volumes'];
+
+			// Single Volume
+			} elseif ( econozel_is_volume() ) {
+
+				// Prepend Volume archives
+				array_splice( $crumbs, $last, 0, array( $_crumbs['volumes'] ) );
+
+				// Set Volume crumb. Overwrites default crumb on queried term.
+				$crumbs[ $last + 1 ] = array(
+					'text'       => econozel_get_volume_title(),
+					'allow_html' => false
+				);
+
+			// Single Edition
+			} elseif ( econozel_is_edition() && $volume = econozel_get_edition_volume() ) {
+
+				// Prepend Volumes archives > {Volume}
+				array_splice( $crumbs, $last, 0, array( $_crumbs['volumes'], array(
+					'text'       => econozel_get_volume_title( $volume, false ),
+					'url'        => econozel_get_volume_url( $volume ),
+					'allow_html' => false
+				) ) );
+
+				// Set Edition crumb. Overwrites default crumb on queried term.
+				$crumbs[ $last + 2 ] = array(
+					'text'       => econozel_get_edition_title(),
+					'allow_html' => false
+				);
+
+			// Article Archive
+			} elseif ( econozel_is_article_archive() ) {
+
+				// Set Article archives crumb. Overwrited default archive crumb.
+				$crumbs[ $last ] = $_crumbs['articles'];
+
+			// Single Article
+			} elseif ( econozel_is_article() ) {
+
+				// Published in Edition
+				if ( $edition = econozel_get_article_edition() ) {
+
+					// Get more ancestry details
+					$volume  = econozel_get_article_volume();
+					$numeric = econozel_is_edition_issue_numeric( $edition );
+
+					// Prepend Volume archives
+					array_splice( $crumbs, $last, 0, array( $_crumbs['volumes'],
+
+						// Volume
+						array(
+							'text'       => econozel_get_volume_title( $volume, false ),
+							'url'        => econozel_get_volume_url( $volume ),
+							'allow_html' => false
+						),
+
+						// Edition
+						array(
+							'text'       => $numeric ? econozel_get_edition_issue( $edition ) : econozel_get_edition_title( $edition ),
+							'url'        => econozel_get_edition_url( $edition ),
+							'allow_html' => false
+						),
+					) );
+
+					// Remove Article archives
+					unset( $crumbs[ $last - 1 ] );
+
+				// Openly published
+				} else {
+
+					// Set Volume archives crumb. Overwrites default archive crumb.
+					$crumbs[ $last - 1 ] = $_crumbs['articles'];
+				}
+			}
+
+			$crumbs = array_values( $crumbs );
+		}
+
+		return $crumbs;
 	}
 }
 
