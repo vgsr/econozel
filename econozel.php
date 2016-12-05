32fb2fea869e04ba0b5bacab13fdf84b1298a5fa
Laurens Offereins <lmoffereins@gmail.com>
2016-12-05 16:40:06 +0100

    Update to version 1.0.0-beta1

--- econozel.php
+++ econozel.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Econozel
  * Description:       Present d' Econozel on your site with articles, editions, and volumes.
  * Plugin URI:        https://github.com/vgsr/econozel/
- * Version:           1.0.0
+ * Version:           1.0.0-beta1
  * Author:            Laurens Offereins
  * Author URI:        https://github.com/vgsr/
  * Text Domain:       econozel
@@ -71,7 +71,7 @@ final class Econozel {
 
 		/** Versions **********************************************************/
 
-		$this->version      = '1.0.0';
+		$this->version      = '1.0.0-beta1';
 
 		/** Paths *************************************************************/
 
