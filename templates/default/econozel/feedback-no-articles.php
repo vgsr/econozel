0415dc3cd9df24cac23771d2ecb1c7f62ace1aed
Laurens Offereins <lmoffereins@gmail.com>
2018-05-25 00:43:52 +0200

    Templates: add missing feedback template parts

---
+++ templates/default/econozel/feedback-no-articles.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * No Articles Feedback Template Part
+ *
+ * @package Econozel
+ * @subpackage Theme
+ */
+
+?>
+
+<p class="econozel-template-notice">
+	<?php esc_html_e( 'Oh bother! There were no Articles found here.', 'econozel' ); ?>
+</p>
---
+++ templates/default/econozel/feedback-no-volumes.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * No Volumes Feedback Template Part
+ *
+ * @package Econozel
+ * @subpackage Theme
+ */
+
+?>
+
+<p class="econozel-template-notice">
+	<?php esc_html_e( 'Oh bother! There were no Volumes found here.', 'econozel' ); ?>
+</p>
